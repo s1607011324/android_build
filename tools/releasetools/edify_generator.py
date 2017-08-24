0b21a9a03f359e85df185968800d3ce0b020979c
Simon Shields <simon@lineageos.org>
2017-08-24 20:52:30 +1000

    releasetools: don't assert device
    this is broken on twrp
    
    Change-Id: I50b52e711cea1a45327d537854c9cf299ea96f9c

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -145,13 +145,7 @@ class EdifyGenerator(object):
 
   def AssertDevice(self, device):
     """Assert that the device identifier is the given string."""
-    cmd = ('assert(' +
-           ' || '.join(['getprop("ro.product.device") == "%s" || getprop("ro.build.product") == "%s"'
-                         % (i, i) for i in device.split(",")]) +
-           ' || abort("This package is for device: %s; ' +
-           'this device is " + getprop("ro.product.device") + ".");' +
-           ');') % device
-    self.script.append(cmd)
+    pass
 
   def AssertSomeBootloader(self, *bootloaders):
     """Asert that the bootloader version is one of *bootloaders."""
